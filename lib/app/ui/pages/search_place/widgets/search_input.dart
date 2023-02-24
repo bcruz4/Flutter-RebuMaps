dcea7ad5b1f4e9e9bba431e3b11b8828b6defa19
bcruz4 <brayandelonge182@gmail.com>
2023-02-24 10:58:55 -0400

    Definiendo origen y destino pt.3, boton de limpiar busqueda previa en origen y destino.

--- lib/app/ui/pages/search_place/widgets/search_input.dart
+++ lib/app/ui/pages/search_place/widgets/search_input.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SearchInput extends StatelessWidget {
+class SearchInput extends StatefulWidget {
   final TextEditingController controller;
   final FocusNode focusNode;
   final String placeholder;
@@ -16,26 +16,65 @@ class SearchInput extends StatelessWidget {
   });
 
   @override
+  State<SearchInput> createState() => _SearchInputState();
+}
+
+class _SearchInputState extends State<SearchInput> {
+  late ValueNotifier<String> _text;
+
+  @override
+  void initState() {
+    super.initState();
+    //valueNotifier renderiza el espacio par alimpiar lo escrityo en el TextFile
+    _text = ValueNotifier(widget.controller.text);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextField(
-        controller: controller,
-        focusNode: focusNode,
-        onChanged: onChanged,
+        controller: widget.controller,
+        focusNode: widget.focusNode,
+        onChanged: (text) {
+          _text.value = text;
+          widget.onChanged!(text);
+        },
         decoration: InputDecoration(
-          labelText: placeholder,
+          labelText: widget.placeholder,
           border: const OutlineInputBorder(),
+          suffixIcon: ValueListenableBuilder<String>(
+            valueListenable: _text,
+            builder: (_, text, child) {
+              if (text.isNotEmpty) {
+                return child!;
+              }
+              return const SizedBox();
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 5),
+              child: CupertinoButton(
+                  padding: const EdgeInsets.all(10),
+                  color: Colors.black38,
+                  child: const Icon(Icons.close_rounded),
+                  onPressed: () {
+                    //limpiar el campo
+                    widget.controller.text = '';
+                    _text.value = '';
+                    widget.onChanged!('');
+                  }),
+            ),
+          ),
         ),
-        // placeholder: placeholder,
-        // decoration: BoxDecoration(
-        //   color: Colors.black12,
-        //   border: Border.all(
-        //     width: 1,
-        //     color: Colors.black12,
-        //   ),
-        // ),
       ),
+      // placeholder: placeholder,
+      // decoration: BoxDecoration(
+      //   color: Colors.black12,
+      //   border: Border.all(
+      //     width: 1,
+      //     color: Colors.black12,
+      //   ),
+      // ),
     );
   }
 }
